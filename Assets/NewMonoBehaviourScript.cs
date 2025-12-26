86ffa105b3d01c038b38a151356a578040c68fec
qwq-vegetabledog <76025572+qwq-vegetabledog@users.noreply.github.com>
2025-12-26 17:22:25 +0100

    Make ground and insert the music

---
+++ Assets/NewMonoBehaviourScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BallController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float forwardSpeed = 10f; //speed of the ball moving forward
+    public float sideSpeed = 15f;//speed of the ball moving sideways
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
